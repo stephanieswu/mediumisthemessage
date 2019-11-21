7e35fd240cafb435ea83b12deab82b168d651472
Stephanie <stephaniew.0818@gmail.com>
2019-11-20 23:29:37 -0500

    2

--- sketch.js
+++ sketch.js
@@ -2,18 +2,26 @@ let s = "Media, by altering the environment, evoke in us unique ratios of sense 
 
 let s2 ="The dominant organ of sensory and social orientation in pre-alphabet societies was the ear– “hearing was believing.” The phonetic alphabet forced the magic world of the ear to yield to the neutral world of the eye. Humans were given an eye for an ear."
 
-let s3 = "Western history was shaped for some three thousand years by the introduction of the phonetic alphabet, a medium that depends solely on the eye for comprehension. The alphabet is a construct of fragmented bits and parts which have no semantic meaning in themselves, and which must be strung together in a line, bead-like, and in a prescribed order. Its use fostered and encouraged the habit of perceiving all environment in visual and spatial terms–particularly in terms of a space and of a time that are uniform, c,o,n,t,i,n,u,o,u,s and c-o-n-n-e-c-t-e-d."
+let s3 = "Western history was shaped for some three thousand years by the introduction of the phonetic alphabet, a medium that depends solely on the eye for comprehension. The alphabet is a construct of fragmented bits and parts which have no semantic meaning in themselves, and which must be strung together in a line, bead-like, and in a prescribed order."
 
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(900, 600);
 }
 
 function draw() {
   background(0);
   pattern();
-  text(s, 50,50,200,500);
-  text(s2, 250,50,250,500);
-  text(s3, 500,50,250,500);
+
+  if(mouseX && mouseY <= 100){
+    text(s, mouseX,mouseY,250,500);
+  }else if (mouseX && mouseY <= 300){
+   text(s2, mouseX,mouseY,250,500);
+  }else if (mouseX && mouseY <= 400){
+   text(s3, mouseX,mouseY,250,500);
+  }else{
+     ellipse(mouseX,mouseY,20,20)
+  }
+
 
 
 }
