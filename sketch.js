748fc4c85d080060513852ddb0e9cc17ef927749
Stephanie <stephaniew.0818@gmail.com>
2019-11-21 14:47:34 -0500

    4

--- sketch.js
+++ sketch.js
@@ -15,20 +15,30 @@ function draw() {
   background(0);
   pattern();
 
+if(mouseIsPressed){
+  for(let i = 0 ; i<= width; i += 15 ){
+    for(let y = 0; y<=height; y += 15 ){
+      ellipse(i,y,10);
+    }
+  }
+}
 
   if(mouseX && mouseY <= 50){
     text(s, mouseX,mouseY,250,500);
-
-  }else if (mouseX && mouseY <= 250){
+ textFont('Courier New');
+}else if (mouseX && mouseY <= 400){
    text(s2, mouseX,mouseY,250,500);
- }else if (mouseX && mouseY <= 350){
+   textFont('Helvetica');
+ }else if (mouseX && mouseY <= 500){
    text(s3, mouseX,mouseY,250,500);
    textSize(14);
+   textFont('Geogria');
  }else if(mouseX && mouseY <= 600){
      text(s4, mouseX,mouseY,50,100);
      textSize(30);
      stroke(0);
      strokeWeight(5)
+      textFont('Arial');
   }else {
     ellipse(mouseX,mouseY,20,20)
   }
@@ -37,12 +47,13 @@ function draw() {
 
 }
 function pattern(){
-  for(let i = 0 ; i<= mouseX; i += 30 ){
-    for(let y = 0; y<= mouseY; y += 30 ){
-      //ellipse(i,y,10);
-      text('media',i,y,200,200)
 
-      fill(i,150,y);
+  for(let i = 0 ; i<= mouseX; i += 50 ){
+    for(let y = 0; y<= mouseY; y += 50 ){
+      //ellipse(i,y,10);
+      text('eye ear',i,y,200,200)
+
+      fill(i,75,y);
     }
   }
 }
